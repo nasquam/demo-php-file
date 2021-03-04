62e66c2a2f02dbb3262de78dbd8710bc37d2cecf
nasquam <sma92878@gmail.com>
2021-03-04 10:14:53 -0500

    updates

--- phpinfo.php
+++ phpinfo.php
@@ -1,22 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<h1>
-    Welcome to the agoge testing PHP app.
-</h1>    
-
-<p>
-    <?php
-        phpinfo()
-    ?>
-</p>
-
-
-</body>
-</html>
+<?php
+    phpinfo()
+?>
